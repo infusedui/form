9a9e5b516cbc8ddc98271f2cf94a83bc81b1b336
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-06-04 17:44:47 +0200

    add password

---
+++ src/components/password/Password.types.ts
@@ -0,0 +1,15 @@
+export interface PasswordProps {
+  label: string;
+  content: string;
+  setContent: React.Dispatch<React.SetStateAction<string>>;
+  error: string;
+  size?: number;
+  readOnly?: boolean;
+  tagline?: {
+    link: string;
+    label: string;
+  };
+  isNew?: boolean;
+  placeHolder?: string;
+  locked?: boolean;
+}
