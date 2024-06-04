109c4db0d97c7f2a27570ab8805d4271bb936489
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-06-04 17:44:04 +0200

    add datepicker

---
+++ src/components/datepicker/DatePicker.types.ts
@@ -0,0 +1,27 @@
+export interface DayArrayProps {
+  day: number;
+  disabled?: boolean;
+}
+
+export type DisabledOption =
+  | "monday"
+  | "tuesday"
+  | "wednesday"
+  | "thursday"
+  | "friday"
+  | "saturday"
+  | "sunday"
+  | "weekend";
+
+export interface DatePickerProps {
+  content: string;
+  setContent: React.Dispatch<React.SetStateAction<string>>;
+  error: string;
+  size?: number;
+  readOnly?: boolean;
+  locked?: boolean;
+  required?: boolean;
+  label?: string;
+  blockedDate?: string[];
+  disabled?: DisabledOption[];
+}
