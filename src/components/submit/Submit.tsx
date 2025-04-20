9594dbe1476d512f09182b47cdffec35ae993677
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-04-20 11:42:22 +0200

    Update Submit.tsx

--- src/components/submit/Submit.tsx
+++ src/components/submit/Submit.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { SubmitProps } from "./Submit.types";
 import { Loader } from "@infusedui/core";
 import "@infusedui/core/dist/infusedui.style.css";
@@ -6,49 +5,24 @@ import "@infusedui/core/dist/infusedui.style.css";
 const Submit: React.FC<SubmitProps> = ({
   size,
   label,
-  otherAction,
   loading,
   locked,
   className,
+  children,
 }) => {
   return (
     <div
-      className={`teaui form-container form-submit ${
-        size ? `block-size-${size}x` : ""
-      } ${className}`}
+      className={
+        size
+          ? `infusedui grid template-cta-container ha-end size-${size}x ${className}`
+          : `infusedui grid template-cta-container ha-end ${className}`
+      }
     >
-      {otherAction && (
-        <Link
-          to={otherAction.link}
-          className={`teaui cta level-tertiary ${
-            !otherAction.iconHide
-              ? otherAction.visible
-                ? "format-icon-left"
-                : "format-icon-only xs-format-icon-left"
-              : ""
-          }`}
-        >
-          {otherAction.iconHide ? (
-            ""
-          ) : (
-            <i
-              className={`icon teaui-icon-${
-                otherAction.icon ? otherAction.icon : "arrow-left"
-              }`}
-            ></i>
-          )}
-          <span className={otherAction.visible ? "" : "viewable-phone"}>
-            {otherAction.label}
-          </span>
-        </Link>
-      )}
-
-      <button
-        className="teaui cta level-primary color-main format-icon-right"
-        disabled={loading || locked ? true : false}
-      >
+      {children}
+      <button disabled={loading || locked ? true : false}>
         <span>{label}</span>
-        {loading ? <Loader /> : <i className="icon teaui-icon-arrow-right"></i>}
+        <i className="icon teaui-icon-arrow-right"></i>
+        {loading && <Loader />}
       </button>
     </div>
   );
